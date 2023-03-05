12fcfe76f36e3ba7bddb20c3c7632a6e14dc9685
Atharva patle <atharvapatle001@gmail.com>
2023-03-05 19:48:01 +0530

    f

--- practice_cp.cpp
+++ practice_cp.cpp
@@ -3,25 +3,16 @@ using namespace std;
 
 int main()
 {
-   vector<int>  que;
-   que.push_back(13);
-   que.push_back(45);
-   que.push_back(12);
-   vector<int> ans;
-  
-   for(int i=que.size()-1;i>=0;i--){
-    int a=que[i];
-    while (a>0)
-    {
-        int c=a%10;
-        ans.push_back(c);
-        a=a/10;
+    string s="codeforces";
+    int t;
+    cin>>t;
+    while(t--){
+     char x;
+     cin>>x;
+     if(find(s.begin(),s.end(),x)!=s.end()){
+        cout<<"Yes";
+        cout<<endl;
+     }
+     else cout<<"NO"<<endl;
     }
-    
-   }
-   reverse(ans.begin(),ans.end());
-   for(int k:ans){
-    cout<<k<<" ";
-   }
- 
 }
